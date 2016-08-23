757af6dd8a4ce5c2f03f48a12b9cbc0450b65c32
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-08-23 14:28:14 +0200

    fake ui handler

--- integration-tests/src/test/java/com/canoo/dolphin/integration/ConnectionTest.java
+++ integration-tests/src/test/java/com/canoo/dolphin/integration/ConnectionTest.java
@@ -3,15 +3,23 @@ package com.canoo.dolphin.integration;
 import com.canoo.dolphin.client.ClientConfiguration;
 import com.canoo.dolphin.client.ClientContext;
 import com.canoo.dolphin.client.ClientContextFactory;
-import com.canoo.dolphin.client.javafx.JavaFXConfiguration;
+import org.opendolphin.core.client.comm.UiThreadHandler;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.logging.Level;
 
 public class ConnectionTest {
 
     @Test
     public void testConnection() {
-        ClientConfiguration configuration = new JavaFXConfiguration("http://localhost:8080/todo-app/dolphin");
+        ClientConfiguration configuration = new ClientConfiguration("http://localhost:8080/todo-app/dolphin", new UiThreadHandler() {
+            @Override
+            public void executeInsideUiThread(Runnable runnable) {
+                runnable.run();
+            }
+        });
+        configuration.setDolphinLogLevel(Level.FINE);
         configuration.setConnectionTimeout(10_000L);
         try {
             ClientContext context = ClientContextFactory.connect(configuration).get();
